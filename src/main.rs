540b202108dfea81db653bd8a82ef400907a2761
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2023-02-07 19:19:11 -0800

    rewrite project using fantoccini because the source requires javascript

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::Command;
+
+use fantoccini::{ClientBuilder, wd::Capabilities};
+use serde_json::json;
+
+const LOGIN_URL: &str = "https://ps.seattleschools.org/public/home.html";
+const GRADES_URL: &str = "https://ps.seattleschools.org/guardian/home.html";
+
+#[tokio::main]
+async fn main() {
+    Command::new("geckodriver").spawn().unwrap();
+
+    get_grades().await.unwrap();
+}
+
+async fn get_grades() -> Result<(), Box<dyn std::error::Error>> {
+    let arg = json!({
+        "args": [
+            "-headless",
+        ],
+    });
+
+    let mut capabilities = Capabilities::new();
+    capabilities.insert(String::from("moz:firefoxOptions"), arg);
+
+    let client = ClientBuilder::native()
+        .capabilities(capabilities)
+        .connect("http://127.0.0.1:4444")
+        .await?;
+
+    client.goto(LOGIN_URL).await?;
+
+    let html = client.source().await?;
+
+    println!("{html}");
+
+    Ok(())
 }
